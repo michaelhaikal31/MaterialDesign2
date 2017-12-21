f50f729d1ece2d915f73d23b5553b187a5e3b1d0
michaelhaikal31 <michaelhaikal31@gmail.com>
2017-12-21 20:07:35 +0700

    add button sheet

---
+++ app/src/main/java/com/example/haikal/materialdesign2/Botton_Sheet/Botton_SheetActivity.java
@@ -0,0 +1,15 @@
+package com.example.haikal.materialdesign2.Botton_Sheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haikal.materialdesign2.R;
+
+public class Botton_SheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botton__sheet);
+    }
+}
